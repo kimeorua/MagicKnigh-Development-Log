f05de2da1fa2b67589cfa46ecd0fd11bc2a243b4
kimeorua <96713052+kimeorua@users.noreply.github.com>
2023-10-11 18:07:38 +0900

    Add files via upload

---
+++ Code/PF_MagicKnightGameMode.cpp
@@ -0,0 +1,31 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PF_MagicKnightGameMode.h"
+#include "PF_MagicKnightCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Blueprint/UserWidget.h"
+
+APF_MagicKnightGameMode::APF_MagicKnightGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
+
+void APF_MagicKnightGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	if (IsValid(PlayerWidgetClass))
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), PlayerWidgetClass);
+
+		if (IsValid(CurrentWidget))
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
---
+++ Code/PF_MagicKnightGameMode.h
@@ -0,0 +1,30 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PF_MagicKnightGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APF_MagicKnightGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+private:
+	//-------------UI----------------//
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Widget", Meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<class UUserWidget> PlayerWidgetClass;
+
+	UPROPERTY()
+	class UUserWidget* CurrentWidget;
+	//-------------UI----------------//
+
+public:
+	APF_MagicKnightGameMode();
+	void StartPlay() override;
+
+};
+
+
+
